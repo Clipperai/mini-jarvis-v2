59235d78a216dd9e5b395e80b5b7e850a86815c7
Clipperai <166284550+Clipperai@users.noreply.github.com>
2026-04-24 17:40:17 +0530

    Update commands.py

--- commands.py
+++ commands.py
@@ -1,19 +1,23 @@
 from apps import open_app
-import datetime
 import webbrowser
+import datetime
 from voice import speak
-
+from whatsapp import send_whatsapp_message
+import pywhatkit as kit
 
 
 sites = {
     "google": "https://www.google.com",
     "youtube": "https://www.youtube.com",
     "gmail": "https://www.gmail.com"
-}
+}    
 
 def run_command(command):
     try: 
-        if command.startswith("open"):
+        if command.startswith("play"):
+            kit.playonyt(command)  #type: ignore
+
+        elif command.startswith("open"):
             command = command.replace('open', ' ').strip()
             if command in sites:
               webbrowser.open(sites[command])
@@ -21,19 +25,19 @@ def run_command(command):
             else:
               open_app(command)
         
-        elif "time" in command:
-            now = datetime.datetime.now()
-            print("The Current time is " + now.strftime("%H:%M"))
-            speak("The Current time is " + now.strftime("%H:%M"))
+        # elif "time" in command:
+        #     now = datetime.datetime.now()
+        #     print("The Current time is " + now.strftime("%I:%M %p"))
+        #     speak("The Current time is " + now.strftime("%I:%M %p"))
         
-        elif "hello" in command:
-            speak("Hello Sir, how can I assist you?")
-
-
+        
         elif "search" in command:
             search_query = command.replace("search", "")
             webbrowser.open("https://www.google.com/search?q=" + search_query)
             speak("Searching for " + search_query)
+        
+        elif "send whatsapp message" or 'send message' in command:
+            send_whatsapp_message()
     
     except:
         return ""
